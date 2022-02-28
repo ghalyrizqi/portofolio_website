b820d112c0bfaf4ba1ddc177ee224533819beb40
Ghaly Rizqi Mauludin <ghalyrizqi@Ghalys-MacBook-Air.local>
2022-02-28 20:22:15 +0700

    first commit

---
+++ data/socials.ts
@@ -0,0 +1,36 @@
+import { Github } from '@/components/icons';
+import { Linkedin } from '@/components/icons';
+import { Twitter } from '@/components/icons';
+import { Youtube } from '@/components/icons';
+
+export const github = {
+  icon: Github,
+  href: 'https://github.com/ghalyrizqi',
+  name: 'github',
+  color: null,
+};
+
+export const linkedin = {
+  icon: Linkedin,
+  href: 'https://www.linkedin.com/in/ghalyrizqi/',
+  name: 'linkedin',
+  color: '#0A66C2',
+};
+
+export const twitter = {
+  icon: Twitter,
+  href: 'https://twitter.com/xghalyrizqix',
+  name: 'twitter',
+  color: '#1DA1F2',
+};
+
+export const youtube = {
+  icon: Youtube,
+  href: 'https://www.youtube.com/',
+  name: 'youtube',
+  color: '#FF0000',
+};
+
+const socials = [github, linkedin, twitter, youtube];
+
+export default socials;
